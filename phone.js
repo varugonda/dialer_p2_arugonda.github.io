b02b06f97e054ab6ab319c848e2817dc4e765299
Vamshi Arugonda <36603288+varugonda@users.noreply.github.com>
2019-02-13 14:05:27 -0700

    Add files via upload

--- phone.js
+++ phone.js
@@ -1,26 +1,41 @@
 $(document).ready(function() { // do this when the document is loaded
 	$("#dialpad").show(); // show the element with ID "dialpad"
 	$("#contacts").hide(); // hide the element with ID "contacts"
-  $("#addcontact").hide(); // hide the element with ID "addcontact"
+	$("#addcontact").hide(); // hide the element with ID "addcontact"
+	$("#help").hide();
 });
 
-$("#dialerButton").click(function() { // do this when dialer is clicked
+function switch2dialer(){
 	$("#dialpad").show(); // show the element with ID "dialpad"
 	$("#contacts").hide(); // hide the element with ID "contacts"
-  $("#addcontact").hide(); // hide the element with ID "addcontact"
-});
+	$("#addcontact").hide(); // hide the element with ID "addcontact"
+	$("#help").hide();
+}
+$("#dialerButton").click(switch2dialer)
 
-$("#phonebookButton").click(function() { // do this when contacts is clicked
+function switch2phonebook(){
 	$("#dialpad").hide(); // hide the element with ID "dialpad"
 	$("#contacts").show(); // show the element with ID "contacts"
-  $("#addcontact").hide(); // hide the element with ID "addcontact"
-});
+	$("#addcontact").hide(); // hide the element with ID "addcontact"
+	$("#help").hide();
+}
+$("#phonebookButton").click(switch2phonebook);
 
-$("#addcontactButton").click(function() { // do this when add contacts is clicked
+function switch2addcontact(){
 	$("#dialpad").hide(); // hide the element with ID "dialpad"
 	$("#contacts").hide(); // hide the element with ID "contacts"
   $("#addcontact").show(); // show the element with ID "addcontact"
-});
+	$("#help").hide();
+}
+$("#addcontactButton").click(switch2addcontact);
+
+function switch2help(){
+	$("#dialpad").hide(); // hide the element with ID "dialpad"
+	$("#contacts").hide(); // show the element with ID "contacts"
+	$("#addcontact").hide(); // hide the element with ID "addcontact"
+	$("#help").show();
+}
+$("#helptab").click(switch2help);
 
 $(".num").click(function(e) {
 	console.log(e.target.id)
@@ -31,7 +46,6 @@ $(".num").click(function(e) {
 
 $("#clear").click(function(){
 	$("#dialed").val("")
-
 });
 
 $("#clear1").click(function(){
@@ -39,3 +53,15 @@ $("#clear1").click(function(){
 	$("#phonenumber").val("")
 	$("#email").val("")	
 });
+
+// $(document).keypress(function(e){
+// 	console.log(e.which)
+// });
+
+hotkeys("d", switch2dialer)
+hotkeys("p", switch2phonebook)
+hotkeys("a", switch2addcontact)
+hotkeys("h", switch2help)
+
+
+
